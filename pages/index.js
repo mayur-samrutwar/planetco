844558b9ad67efbc26f7a630bd721691b14f8df5
mayur-samrutwar <samrutwarmayur1@gmail.com>
2024-04-01 01:53:00 +0530

    update: add title

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Image from "next/image";
 import PlanetCoLogo from '../assets/planetco_logo.png'
 import { Inter } from "next/font/google";
@@ -7,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <section id="homePage" className="">
+      <Head>
+        <title>Restoring the Planet, one city at a time | PlanetCo</title>
+      </Head>
       <div className="min-h-screen w-full flex justify-center items-center">
         <Image src={PlanetCoLogo} className="w-1/4 rounded-full" />
       </div>
